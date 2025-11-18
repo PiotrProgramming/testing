eb2ad9221dd8abe48c223727fa8337886c346955
PiotrProgramming <piotr.stanisztransport@gmail.com>
2025-11-18 15:49:43 +0100

    Create videos.js

---
+++ src/config/videos.js
@@ -0,0 +1,9 @@
+import video1 from '@/videos/video1.mp4';
+import video2 from '@/videos/video2.mp4';
+import video3 from '@/videos/video3.mp4';
+
+export const videos = [
+  video1,
+  video2,
+  video3,
+];
